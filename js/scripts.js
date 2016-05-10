852656471fe124bc339de8276ecdbd38a396f446
curtjenk <curtjenk@comcast.net>
2016-05-10 16:45:54 -0400

    ...

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
-$(document).ready(function() {
-    console.log("doc ready");
-    $(".mg-tile-outside").hover(
-        function() {
-            console.log("mouseenter");
+$(document).ready(function () {
+	console.log("doc ready");
+	$(".mg-tile-outside").hover(
+		function () {
+			console.log("mouseenter");
 
-            $(this).siblings(".mg-tile-inside").css('display', 'block');
-            $(this).siblings(".mg-tile-inside").slideDown(2000);
-            // $(this).siblings(".mg-tile-inside").animate({top: "+=200"}, "slow");
-        },
-        function() {
-            console.log("mouseleave");
-            $(this).siblings(".mg-tile-inside").slideUp(2000);
-            console.log("animation complete");
-            $(this).siblings(".mg-tile-inside").css('display', 'none');
-        });
+			$(this).siblings(".mg-tile-inside").css('display', 'block');
+			$(this).siblings(".mg-tile-inside").slideDown(2000);
+			// $(this).siblings(".mg-tile-inside").animate({top: "+=200"}, "slow");
+		},
+		function () {
+			console.log("mouseleave");
+			$(this).siblings(".mg-tile-inside").slideUp(2000);
+			console.log("animation complete");
+			$(this).siblings(".mg-tile-inside").css('display', 'none');
+		});
 });
 
 
@@ -43,30 +43,30 @@ $window.on('scroll resize', check_if_in_view);
 $window.trigger('scroll');
 
 function check_if_in_view() {
-    var $camp = $('#reboot');
-    if ($camp.is(':in-viewport( 0 )')) {
-        // console.log("scrolled into viewport");
-        $camp.find('.skill-card').css('visibility', 'visible');
-        $camp.find('.skill-card').addClass('animated');
+	var $camp = $('#reboot');
+	if ($camp.is(':in-viewport( 0 )')) {
+		// console.log("scrolled into viewport");
+		$camp.find('.skill-card').css('visibility', 'visible');
+		$camp.find('.skill-card').addClass('animated');
 
-        $camp.find('.skill-card.left').removeClass('fadeOutLeft');
-        $camp.find('.skill-card.middle').removeClass('fadeOutDownBig');
-        $camp.find('.skill-card.right').removeClass('fadeOutRight');
+		$camp.find('.skill-card.left').removeClass('fadeOutLeft');
+		$camp.find('.skill-card.middle').removeClass('fadeOutDownBig');
+		$camp.find('.skill-card.right').removeClass('fadeOutRight');
 
-        $camp.find('.skill-card.left').addClass('fadeInLeft');
-        $camp.find('.skill-card.middle').addClass('fadeInUpBig');
-        $camp.find('.skill-card.right').addClass('fadeInRight');
-    } else {
-        // console.log("scrolled out of viewport");
-        $camp.find('.skill-card').css('visibility', 'hidden');
-        $camp.find('.skill-card').removeClass('animated');
+		$camp.find('.skill-card.left').addClass('fadeInLeft');
+		$camp.find('.skill-card.middle').addClass('fadeInUpBig');
+		$camp.find('.skill-card.right').addClass('fadeInRight');
+	} else {
+		// console.log("scrolled out of viewport");
+		$camp.find('.skill-card').css('visibility', 'hidden');
+		$camp.find('.skill-card').removeClass('animated');
 
-        $camp.find('.skill-card.left').removeClass('fadeInLeft');
-        $camp.find('.skill-card.middle').removeClass('fadeInUpBig');
-        $camp.find('.skill-card.right').removeClass('fadeInRight');
+		$camp.find('.skill-card.left').removeClass('fadeInLeft');
+		$camp.find('.skill-card.middle').removeClass('fadeInUpBig');
+		$camp.find('.skill-card.right').removeClass('fadeInRight');
 
-        $camp.find('.skill-card.left').addClass('fadeOutLeft');
-        $camp.find('.skill-card.middle').addClass('fadeOutDownBig');
-        $camp.find('.skill-card.right').addClass('fadeOutRight');
-    }
+		$camp.find('.skill-card.left').addClass('fadeOutLeft');
+		$camp.find('.skill-card.middle').addClass('fadeOutDownBig');
+		$camp.find('.skill-card.right').addClass('fadeOutRight');
+	}
 }
